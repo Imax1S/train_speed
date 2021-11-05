9b9c3464a39be1456e310a29b9ecbf5414ba746d
IMAX <maxim.ionko@gmail.com>
2021-11-05 14:40:00 +0300

    base

---
+++ app/src/main/java/com/example/train_speed/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.train_speed
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.train_speed.ui.theme.Train_speedTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Train_speedTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Train_speedTheme {
+        Greeting("Android")
+    }
+}
