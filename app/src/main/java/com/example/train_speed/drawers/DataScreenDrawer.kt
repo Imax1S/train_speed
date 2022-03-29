97370ad1b1881a635fb74026ae80370850a0a6e7
ImaxIS <m.ionko@tinkoff.ru>
2022-03-29 22:15:14 +0300

    fixed data screen UI

--- app/src/main/java/com/example/train_speed/drawers/DataScreenDrawer.kt
+++ app/src/main/java/com/example/train_speed/drawers/DataScreenDrawer.kt
@@ -1,21 +1,34 @@
 package com.example.train_speed.drawers
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Card
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.train_speed.model.SpeedMeasurement
 import com.example.train_speed.view_models.DataScreenViewModel
 
 class DataScreenDrawer(private val dataScreenViewModel: DataScreenViewModel) {
+    private val padding = 16.dp
 
     @Composable
     fun DataScreen() {
 
         val measurements by dataScreenViewModel.items.observeAsState()
-        Column {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding),
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.Top
+        ) {
             measurements?.forEach { measurement ->
                 MeasurementRow(measurement)
             }
@@ -24,11 +37,18 @@ class DataScreenDrawer(private val dataScreenViewModel: DataScreenViewModel) {
 
     @Composable
     fun MeasurementRow(measurement: SpeedMeasurement) {
-        Card {
-            Column {
+        val date = measurement.date
+
+        Button(
+            modifier = Modifier
+                .padding(vertical = 8.dp),
+            onClick = {}
+        ) {
+            Column(
+            ) {
                 Text(text = measurement.title)
                 Text(text = "Avr. Speed: ${measurement.avgSpeed}")
-                Text(text = measurement.date.toString())
+                Text(text = "Date: $date")
             }
         }
     }
